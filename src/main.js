af2f1eafb3ffdb5cad41ac5794287e90cdf07312
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-05-09 22:07:11 -0600

    Consumo de API para mostrar la lista de generos de peliculas

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 console.log('API URL = https://loquesea.com/cualquierEndpoint?api_key=' + API_KEY);
 
-async function getTrendingMoviesPreview () {
+async function getTrendingMoviesPreview () {                                                          // Hace petición para recibir las 20 peliculas en tendencias y genera HTML para mostrarlas en un slide usando ".forEach()"
   const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
   const data = await res.json();
-  const movies = data.results;                                  // Guardamos en la variable "movies" la propiedad de nuestra respuestas que se llama ".results" la otra propiedad es ".page"
+  const movies = data.results;                                                                        // Guardamos en la variable "movies" la propiedad de nuestra respuestas que se llama ".results" la otra propiedad es ".page"
   movies.forEach(movie => {
     const trendingMoviesPreviewContainer = document.querySelector
-    ('#trendingPreview .trendingPreview-movieList');           // Para seleccionar al elemento interno puedo usar los selectores como en CSS donde indico a cualquier elemento ('.trendingPreview-movieList') que se encuentre en su padre ('#trendingPreview')
+    ('#trendingPreview .trendingPreview-movieList');                                                  // Para seleccionar al elemento interno puedo usar los selectores como en CSS donde indico a cualquier elemento ('.trendingPreview-movieList') que se encuentre en su padre ('#trendingPreview')
 
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
@@ -20,10 +20,38 @@ async function getTrendingMoviesPreview () {
 
     movieContainer.appendChild(movieImg);
     trendingMoviesPreviewContainer.appendChild(movieContainer);
-
-
   });
 }
 
 
+async function getCategoriesPreview() {                                                               // Consume una petición que retorna las categorias de pelicular y crea los elementos HTML para mostrarlos en el FrontEnd usando ".forEach()"
+  const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`)
+  const data = await res.json();
+  const categories = data.genres;
+  categories.forEach(category => {
+    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+    
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id', `id${category.id}`);
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+  });
+}
+
+
+
+
+
+
+
+
+
 getTrendingMoviesPreview();
+getCategoriesPreview();
