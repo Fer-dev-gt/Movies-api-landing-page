f59103ec6474bc498fe1877e39d25d47e185a739
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-05-09 21:50:53 -0600

    Consume de la API para mostrar las peliculas en Tendencias

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-console.log('API URL = https://luhiuhdfasd.com/dfjaslkfds?api_key=' + API_KEY);
+console.log('API URL = https://loquesea.com/cualquierEndpoint?api_key=' + API_KEY);
+
+async function getTrendingMoviesPreview () {
+  const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
+  const data = await res.json();
+  const movies = data.results;                                  // Guardamos en la variable "movies" la propiedad de nuestra respuestas que se llama ".results" la otra propiedad es ".page"
+  movies.forEach(movie => {
+    const trendingMoviesPreviewContainer = document.querySelector
+    ('#trendingPreview .trendingPreview-movieList');           // Para seleccionar al elemento interno puedo usar los selectores como en CSS donde indico a cualquier elemento ('.trendingPreview-movieList') que se encuentre en su padre ('#trendingPreview')
+
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute('src', 
+    `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
+    );
+
+    movieContainer.appendChild(movieImg);
+    trendingMoviesPreviewContainer.appendChild(movieContainer);
+
+
+  });
+}
+
+
+getTrendingMoviesPreview();
