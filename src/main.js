0f71099bbeaa5668d77dd969c681fde7ab548f68
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-05-10 00:32:05 -0600

    Implementación de Axios dentro de funciones existentes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-console.log('API URL = https://loquesea.com/cualquierEndpoint?api_key=' + API_KEY);
+const api = axios.create({
+  baseURL : 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {                                                             // A parte de enviar mis API_KEY por los "headers", puedo usar "params" e indicar cualos son los parámetros que podemos agregar, siempre en formato de objeto
+    'api_key': API_KEY,
+  }
+})
 
 async function getTrendingMoviesPreview () {                                                          // Hace petición para recibir las 20 peliculas en tendencias y genera HTML para mostrarlas en un slide usando ".forEach()"
-  const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
-  const data = await res.json();
+  const { data } = await api(`trending/movie/day`);                                                   // Hacemos una solicitud usando "Axios", ya tenemos registrada la URL base, ya solo tenemos que definir el "endpoint" en especifico que queremos utilizar
   const movies = data.results;                                                                        // Guardamos en la variable "movies" la propiedad de nuestra respuestas que se llama ".results" la otra propiedad es ".page"
+
   movies.forEach(movie => {
     const trendingMoviesPreviewContainer = document.querySelector
     ('#trendingPreview .trendingPreview-movieList');                                                  // Para seleccionar al elemento interno puedo usar los selectores como en CSS donde indico a cualquier elemento ('.trendingPreview-movieList') que se encuentre en su padre ('#trendingPreview')
@@ -25,9 +33,9 @@ async function getTrendingMoviesPreview () {                                    
 
 
 async function getCategoriesPreview() {                                                               // Consume una petición que retorna las categorias de pelicular y crea los elementos HTML para mostrarlos en el FrontEnd usando ".forEach()"
-  const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`)
-  const data = await res.json();
+  const { data } = await api(`genre/movie/list?language=es`);                            // Al usar Axios ya no tengo que escribir mi URL base ya que ya la declaré             
   const categories = data.genres;
+
   categories.forEach(category => {
     const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
     
